323ccf0c470f2b574dd8644b433165ba8e3af611
Hampson <hampsonj1@elslabm-156684.science.regn.net>
2019-08-12 18:11:06 +0100

    dunno

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Clarifai from 'clarifai';
 
+const app = new Clarifai.App({
+ apiKey: '43921cbcf15440adad9ce85e9a6c7f98'
+});
 
 const particlesOptions = {
   particles: {
@@ -34,7 +39,18 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('click')
+    console.log('click');
+    app.models.predict(
+      Clarifai.COLOR_MODEL,
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
+    function(response) {
+      console.log(response)
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render() {
@@ -47,7 +63,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        {/*<FaceRecognition />*/}
+        <FaceRecognition />}
       </div>
     );
   }
